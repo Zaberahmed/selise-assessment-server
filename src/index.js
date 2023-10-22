e4671fad3a7ec105098574ed0014cb09e83a4fdb
Zaber Ahmed <zaber0305@gmail.com>
2023-10-22 12:55:59 +0600

    Modify: start script

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
-server.listen(port, () => console.log(`Server is running on port:${port}`));
+server.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
